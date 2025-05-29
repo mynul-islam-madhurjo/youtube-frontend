26508deda3aab511158a29d146389e90eaa8848d
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2025-05-29 22:46:21 +0600

    layouts added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['localhost', 'i.ytimg.com', 'yt3.ggpht.com'],
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '8000',
+          pathname: '/static/**',
+        },
+      ],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:8000/api/:path*',
+        },
+      ];
+    },
+  }
+  
+  module.exports = nextConfig
